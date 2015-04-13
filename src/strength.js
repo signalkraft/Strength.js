34557a307e7135a36dd20508ae6658da12c73789
Philipp Dörner <p.doerner@testbirds.de>
2015-04-13 16:08:52 +0200

    simplified indicator

--- src/strength.js
+++ src/strength.js
@@ -1,162 +1,109 @@
 /*!
  * strength.js
  * Original author: @aaronlumsden
- * Further changes, comments: @aaronlumsden
+ * Further changes, comments: @aaronlumsden, github.com/signalkraft
  * Licensed under the MIT license
  */
-;(function ( $, window, document, undefined ) {
+;
+(function ($, window, document, undefined) {
 
     var pluginName = "strength",
         defaults = {
-            strengthClass: 'strength',
-            strengthMeterClass: 'strength_meter',
-            strengthButtonClass: 'button_strength',
-            strengthButtonText: 'Show Password',
-            strengthButtonTextToggle: 'Hide Password'
+            strengthClass: 'strength'
         };
 
-       // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
-
-    function Plugin( element, options ) {
+    function Plugin(element, options) {
         this.element = element;
         this.$elem = $(this.element);
-        this.options = $.extend( {}, defaults, options );
+        this.options = $.extend({}, defaults, options);
         this._defaults = defaults;
         this._name = pluginName;
         this.init();
     }
 
     Plugin.prototype = {
-
-        init: function() {
-
-
+        init: function () {
             var characters = 0;
             var capitalletters = 0;
-            var loweletters = 0;
+            var lowerletters = 0;
             var number = 0;
-            var special = 0;
 
-            var upperCase= new RegExp('[A-Z]');
-            var lowerCase= new RegExp('[a-z]');
+            var upperCase = new RegExp('[A-Z]');
+            var lowerCase = new RegExp('[a-z]');
             var numbers = new RegExp('[0-9]');
-            var specialchars = new RegExp('([!,%,&,@,#,$,^,*,?,_,~])');
 
-            function GetPercentage(a, b) {
-                    return ((b / a) * 100);
-                }
+            var strengths = ['very-weak', 'weak', 'medium', 'strong', 'very-strong'];
 
-                function check_strength(thisval,thisid){
-                    if (thisval.length > 8) { characters = 1; } else { characters = -1; };
-                    if (thisval.match(upperCase)) { capitalletters = 1} else { capitalletters = 0; };
-                    if (thisval.match(lowerCase)) { loweletters = 1}  else { loweletters = 0; };
-                    if (thisval.match(numbers)) { number = 1}  else { number = 0; };
-
-                    var total = characters + capitalletters + loweletters + number + special;
-                    var totalpercent = GetPercentage(7, total).toFixed(0);
-
-                    if (!thisval.length) {total = -1;}
-
-                    get_total(total,thisid);
-                }
-
-            function get_total(total,thisid){
-
-                var thismeter = $('div[data-meter="'+thisid+'"]');
-                    if (total <= 1) {
-                   thismeter.removeClass();
-                   thismeter.addClass('veryweak').html('very weak');
-                } else if (total == 2){
-                    thismeter.removeClass();
-                   thismeter.addClass('weak').html('weak');
-                } else if(total == 3){
-                    thismeter.removeClass();
-                   thismeter.addClass('medium').html('medium');
-
+            function calculateStrength($elem) {
+                var value = $elem.val();
+                if (value.length >= 8) {
+                    characters = 1;
                 } else {
-                     thismeter.removeClass();
-                   thismeter.addClass('strong').html('strong');
+                    characters = -1;
                 }
-                
-                if (total == -1) { thismeter.removeClass().html('Strength'); }
+                if (value.match(upperCase)) {
+                    capitalletters = 1
+                } else {
+                    capitalletters = 0;
+                }
+                if (value.match(lowerCase)) {
+                    lowerletters = 1
+                } else {
+                    lowerletters = 0;
+                }
+                if (value.match(numbers)) {
+                    number = 1
+                } else {
+                    number = 0;
+                }
+
+                var total = characters + capitalletters + lowerletters + number;
+
+                if (!value.length) {
+                    total = -1;
+                }
+
+                return total;
             }
 
+            function setClasses($elem, total) {
+                if (total > -1) {
+                    var strength = strengths[total];
+                    var unused = strengths.filter(function(x) {
+                        if (x != strength) {
+                            return x;
+                        }
+                    });
 
-
-
-
-            var isShown = false;
-            var strengthButtonText = this.options.strengthButtonText;
-            var strengthButtonTextToggle = this.options.strengthButtonTextToggle;
-
-
-            thisid = this.$elem.attr('id');
-
-            this.$elem.addClass(this.options.strengthClass).attr('data-password',thisid).after('<input style="display:none" class="'+this.options.strengthClass+'" data-password="'+thisid+'" type="text" name="" value=""><a data-password-button="'+thisid+'" href="" class="'+this.options.strengthButtonClass+'">'+this.options.strengthButtonText+'</a><div class="'+this.options.strengthMeterClass+'"><div data-meter="'+thisid+'">Strength</div></div>');
-             
-            this.$elem.bind('keyup keydown', function(event) {
-                thisval = $('#'+thisid).val();
-                $('input[type="text"][data-password="'+thisid+'"]').val(thisval);
-                check_strength(thisval,thisid);
-                
-            });
-
-             $('input[type="text"][data-password="'+thisid+'"]').bind('keyup keydown', function(event) {
-                thisval = $('input[type="text"][data-password="'+thisid+'"]').val();
-                console.log(thisval);
-                $('input[type="password"][data-password="'+thisid+'"]').val(thisval);
-                check_strength(thisval,thisid);
-                
-            });
-
-
-
-            $(document.body).on('click', '.'+this.options.strengthButtonClass, function(e) {
-                e.preventDefault();
-
-               thisclass = 'hide_'+$(this).attr('class');
-
-
-
-
-                if (isShown) {
-                    $('input[type="text"][data-password="'+thisid+'"]').hide();
-                    $('input[type="password"][data-password="'+thisid+'"]').show().focus();
-                    $('a[data-password-button="'+thisid+'"]').removeClass(thisclass).html(strengthButtonText);
-                    isShown = false;
-
+                    $elem.addClass(strength).data('password-strength', strength);
+                    $elem.removeClass(unused.join(' '));
                 } else {
-                    $('input[type="text"][data-password="'+thisid+'"]').show().focus();
-                    $('input[type="password"][data-password="'+thisid+'"]').hide();
-                    $('a[data-password-button="'+thisid+'"]').addClass(thisclass).html(strengthButtonTextToggle);
-                    isShown = true;
-   
+                    $elem.removeClass(strengths.join(' '));
+                    $elem.removeData('password-strength');
                 }
+            }
 
+            var $elem = this.$elem;
 
-               
+            this.$elem.bind('keyup keydown', function (event) {
+                var strength = calculateStrength($elem);
+                setClasses($elem, strength);
             });
 
-
-         
-            
-        },
-
-        yourOtherFunction: function(el, options) {
-            // some logic
+            this.$elem.addClass(this.options.strengthClass);
         }
     };
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
+    $.fn[pluginName] = function (options) {
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
             }
         });
     };
 
-})( jQuery, window, document );
+})(jQuery, window, document);
 
 
